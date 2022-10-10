cfc13126bdbb6015e926eed2473bf0fca69c6d46
Kubachoinski <113339346+Kubachoinski@users.noreply.github.com>
2022-10-10 12:16:00 +0200

    4.1

--- blog/polls/admin.py
+++ blog/polls/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 
-from .models import Question
-
-# Register your models here.
-
-
-from django.contrib import admin
-
 from .models import Question, Choice, Answer
 
 class QuestionAdmin(admin.ModelAdmin):
